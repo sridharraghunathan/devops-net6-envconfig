f47af6c5354881bdc7a30d61abf43dc769d836ed
raghavendradpi <sriraghavendraswamydpi@gmail.com>
2023-02-05 21:51:20 +0530

    Add project files.

---
+++ devops-net6-envconfig/Controllers/ConfigController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace devops_net6_envconfig.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConfigController : ControllerBase
+    {
+        public IConfiguration _configuration { get; }
+
+        public ConfigController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+         [HttpGet]
+        public async Task<string> GetConfiguration()
+        {
+            var config = _configuration["SqlConnectionString"];
+            return await Task.Run(() => config);
+        }
+    }
+}
---
+++ devops-net6-envconfig/Program.cs
@@ -0,0 +1,30 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//For getting the Current Environment
+var config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+var environment = config.GetSection("EnvironmentName").Value;
+builder.Configuration.AddJsonFile("appsettings.json").AddJsonFile($"appsettings.{environment}.json");
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
